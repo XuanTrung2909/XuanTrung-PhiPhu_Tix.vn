2d88644dcc739859381c2c4bae4722648857c42c
XuanTrung2909 <trung.nx290989@gmail.com>
2021-02-27 18:20:41 +0700

    modify complete showtimes

--- js/index.js
+++ js/index.js
@@ -6,11 +6,18 @@ document.getElementById("playing").addEventListener("click", function(){
     document.getElementById("playing").className = "active";
     document.getElementById("upcoming__content").style.display = "none";
     document.getElementById("upcoming").className = "";
-
+    document.getElementById("slide__prev__playing").style.display = "inline-block";
+    document.getElementById("slide__next__playing").style.display = "inline-block";
+    document.getElementById("slide__prev__upcoming").style.display = "none";
+    document.getElementById("slide__next__upcoming").style.display = "none";
 })
 document.getElementById("upcoming").addEventListener("click", function(){
     document.getElementById("playing__content").style.display = "none";
     document.getElementById("upcoming__content").style.display = "block";
     document.getElementById("upcoming").className = "active";
     document.getElementById("playing").className = "";
+    document.getElementById("slide__prev__playing").style.display = "none";
+    document.getElementById("slide__next__playing").style.display = "none";
+    document.getElementById("slide__prev__upcoming").style.display = "inline-block";
+    document.getElementById("slide__next__upcoming").style.display = "ninline-block";
 })
